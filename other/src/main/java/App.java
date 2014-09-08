5ba9c1cd800adfb61c9221344fea9feaa06a11cf
canhun <canhun@taobao.com>
2014-09-08 17:15:18 +0800

    add subproject

---
+++ other/src/main/java/App.java
@@ -0,0 +1,9 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2014 All Rights Reserved.
+ *
+ * @author canhun@taobao.com
+ * @version $Id: App.java, v 0.1 2014-09-08 PM5:14:00 canhun Exp $
+ */
+public class App {
+}
